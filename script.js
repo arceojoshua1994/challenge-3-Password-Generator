55408c3ba618464017dbf0e8319a9ba3b28c83b0
arceojoshua94 <arceojoshua94@outlook.com>
2023-07-06 21:53:57 -0400

    save work

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ function generatePassword() {
   var length = prompt("Enter the desired password length (maximum 8 characters):");
   length = parseInt(length);
 
-  if (isNaN(length) || length <= 0 || length > 8) {
-    alert("Invalid password length. Please enter a number between 1 and 8.");
-    return "";
+    if (isNaN(length) || length <= 0 || length > 8) {
+      alert("Invalid password length. Please enter a number between 1 and 8.");
+      return "";
   }
 
   var lowercase = confirm("Include lowercase letters?");
@@ -42,23 +42,23 @@ function generatePassword() {
   var numericChars = "0123456789";
   var specialChars = "!@#$%^&*()";
 
-  if (lowercase) {
-    characters += lowercaseChars;
+    if (lowercase) {
+      characters += lowercaseChars;
   }
-  if (uppercase) {
-    characters += uppercaseChars;
+    if (uppercase) {
+      characters += uppercaseChars;
   }
-  if (numeric) {
-    characters += numericChars;
+    if (numeric) {
+      characters += numericChars;
   }
-  if (special) {
-    characters += specialChars;
+    if (special) {
+      characters += specialChars;
   }
 
   var password = "";
-  for (var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters.charAt(randomIndex);
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters.charAt(randomIndex);
   }
 
   return password;
